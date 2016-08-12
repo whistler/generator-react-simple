2b86a748c05986039e0c24770e79f87362323edb
Ibrahim Muhammad <ibrahim.mohammad@gmail.com>
2016-08-11 22:31:11 -0700

    Copy dot files

--- app/index.js
+++ app/index.js
@@ -3,5 +3,6 @@ var generators = require('yeoman-generator')
 module.exports = generators.Base.extend({
   writing: function() {
     this.fs.copy(this.sourceRoot(), this.destinationRoot())
+    this.fs.copy(this.templatePath('.*'), this.destinationRoot())
   }
 })
